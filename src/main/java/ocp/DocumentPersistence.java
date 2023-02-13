b826430d94f351fdde7c318565ad86e259df56ec
Pawel <pawel.matusiak29@gmail.com>
2023-02-13 14:49:04 +0100

    Merged OCP solution into master

---
+++ src/main/java/ocp/DocumentPersistence.java
@@ -0,0 +1,16 @@
+package ocp;
+
+public class DocumentPersistence {
+
+    private final Document document;
+    private final DocumentSaver saver;
+
+    public DocumentPersistence(Document document, DocumentSaver saver) {
+        this.document = document;
+        this.saver = saver;
+    }
+
+    public void save(){
+        saver.save(this.document);
+    }
+}
